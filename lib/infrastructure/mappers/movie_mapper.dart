59a8fd52ed7521021351abd13b4f19bc1d51f35e
Mateusz Baryla <mateusz.baryla@zollsoft.de>
2023-10-20 22:45:50 +0200

    MovieMapper implemented

---
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -0,0 +1,34 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
+
+class MovieMapper {
+  static Movie movieDBToEntity(MovieMovieDB moviedb) => Movie(
+        adult: moviedb.adult,
+        backdropPath: (moviedb.backdropPath != '')
+            ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
+            : 'https://sd.keepcalms.com/i/keep-calm-poster-not-found.png',
+        genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
+        id: moviedb.id,
+        originalLanguage: moviedb.originalLanguage,
+        originalTitle: moviedb.originalTitle,
+        overview: moviedb.overview,
+        popularity: moviedb.popularity,
+        posterPath: (moviedb.posterPath != '')
+            ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
+            : 'https://sd.keepcalms.com/i/keep-calm-poster-not-found.png',
+        releaseDate: moviedb.releaseDate,
+        title: moviedb.title,
+        video: moviedb.video,
+        voteAverage: moviedb.voteAverage,
+        voteCount: moviedb.voteCount,
+      );
+}
+
+/*
+* El mapper va a tener como única misión leer diferentes
+* modelos y crear mi entidad.
+* El objetivo de mapper es tomar la implementación específica de
+* TheMovieDB y que me sirva a mí para transformarlo a mi entidad.
+* Esta capa, es una capa de protección del API que viene de afuera
+* con nuestra aplicación.
+* */
