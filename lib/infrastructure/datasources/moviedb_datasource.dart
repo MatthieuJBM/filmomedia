030580122b1ee57c6404783526a2a5697555c042
Mateusz Baryla <mateusz.baryla@zollsoft.de>
2023-10-20 23:10:34 +0200

    The implementation MovieDbDatasource of MoviesDatasource implemented. It receives the movies from API and returns our object

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -1,27 +1,41 @@
 import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import '../models/moviedb/moviedb_response.dart';
+
 import 'package:dio/dio.dart';
 
 class MovieDbDatasource extends MoviesDatasource {
   // baseUrl quiere decir que cuando voy a utilizar esta instancia dio
   // todas las peticiones van a tener esa base del URL precargada
   // o preconfigurada.
-  final dio = Dio(BaseOptions(
-    baseUrl: 'https://api.themoviedb.org/3',
-    queryParameters: {
-      'api_key': Environment.theMovieDbKey,
-      'language': 'es-MX',
-    }
-  ));
+  final dio = Dio(
+      BaseOptions(baseUrl: 'https://api.themoviedb.org/3', queryParameters: {
+    'api_key': Environment.theMovieDbKey,
+    'language': 'es-MX',
+  }));
 
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
-    
     final response = await dio.get('/movie/now_playing');
-    //response.data;
-    final List<Movie> movies = [];
-    
-    return [];
+
+    final movieDBResponse = MovieDbResponse.fromJson(response.data);
+
+    final List<Movie> movies = movieDBResponse.results
+        .where((moviedb) => moviedb.posterPath != 'no-poster')
+        .map((moviedb) => MovieMapper.movieDBToEntity(moviedb))
+        .toList();
+
+    return movies;
   }
 }
+
+/*
+* Si no viene el póster, no me interesa esa película. No la quiero
+* mostrar ahí. ¿Cómo hacemos este filtro? Una de las maneras más
+* sencillas es que, como es una lista, la podemos pasar por el método
+* "where()". Esto se como un filtro. Si la condición es "true",
+* lo deja pasar. Si no, entonces simplemente filtra ese resultado.
+*
+* */
